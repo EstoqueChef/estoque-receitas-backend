6d9cbcb640d233bea804508d03724c909f5d332a
lmfreire <l.matheus.freire10@gmail.com>
2025-04-22 12:46:16 -0300

    criação de rota de receitas

--- src/receitas/receitas.controller.ts
+++ src/receitas/receitas.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { ReceitasService } from './receitas.service';
+import { CreateReceitaDto } from './dto/create-receita.dto';
+import { RequestWithUser } from 'src/auth/jwt.strategy';
 
 @Controller('receitas')
-export class ReceitasController {}
+export class ReceitasController {
+
+    constructor(
+        private readonly receitasService: ReceitasService
+    ) { }
+
+    @Post()
+    async create(
+        @Body() data: CreateReceitaDto,
+        @Req() req: RequestWithUser
+    ) {
+        const usuarioId = req.user.sub;
+        return this.receitasService.create(data, usuarioId)
+    }
+
+    @Get()
+    async findAll(@Req() req: RequestWithUser) {
+        const usuarioId = req.user.sub;
+        return this.receitasService.findAll(usuarioId)
+    }
+
+}
--- src/receitas/receitas.service.ts
+++ src/receitas/receitas.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateReceitaDto } from './dto/create-receita.dto';
 
 @Injectable()
-export class ReceitasService {}
+export class ReceitasService {
+
+  constructor(
+    private prisma: PrismaService
+  ) { }
+
+  async create(data: CreateReceitaDto, usuarioId: string) {
+
+    const insumosIds = data.insumos.map(i => i.insumoId);
+    const insumos = await this.prisma.insumo.findMany({
+      where: {
+        id: { in: insumosIds },
+        usuarioId
+      }
+    })
+
+    if (insumos.length !== data.insumos.length) {
+      throw new BadRequestException('Um ou mais insumos são inválidos ou não pertencem ao usuário');
+    }
+
+    return this.prisma.receita.create({
+      data: {
+        nome: data.nome,
+        descricao: data.descricao,
+        usuarioId,
+        insumos: {
+          create: data.insumos.map(insumo => ({
+            insumoId: insumo.insumoId,
+            quantidade: insumo.quantidade
+          }))
+        }
+      },
+      include: {
+        insumos: {
+          include: {
+            insumo: true
+          }
+        }
+      }
+    })
+  }
+
+
+  async findAll(usuarioId: string) {
+
+    return this.prisma.receita.findMany({
+      where: { usuarioId },
+      include: {
+        insumos: {
+          include: {
+            insumo: true
+          }
+        }
+      },
+      orderBy: { nome: 'asc' }
+    })
+  }
+}
